39fa6a9282498d086aee4471e701f7d546581cad
hmlaiac <79184758+hmlaiac@users.noreply.github.com>
2022-12-06 23:56:32 +0800

    Finialize\

--- spring model/demo/src/main/java/com/example/demo/controller/Helloworld.java
+++ spring model/demo/src/main/java/com/example/demo/controller/Helloworld.java
@@ -1,9 +1,8 @@
 package com.example.demo.controller;
 
+import com.example.demo.model.InOb;
 import com.example.demo.model.OutputOb;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/test")
@@ -12,5 +11,15 @@ public class Helloworld {
     public OutputOb output(){
         OutputOb out = new OutputOb("This is a message", 123456789);
         return out;
+    }
+
+    @PostMapping("/in")
+    public InOb input(@RequestBody InOb inOb){
+        return inOb;
+    }
+
+    @GetMapping("/query/{dummy}")
+    public String query(@PathVariable("dummy") String dummy){
+        return dummy;
     }
 }
--- spring model/demo/src/main/java/com/example/demo/model/InOb.java
+++ spring model/demo/src/main/java/com/example/demo/model/InOb.java
@@ -1,8 +1,15 @@
 package com.example.demo.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 @Data
-
 public class InOb {
+    private String message;
+    private String dummy;
+
+    public InOb(@JsonProperty("message") String message, @JsonProperty("dummy") String dummy) {
+        this.message = message;
+        this.dummy = dummy;
+    }
 }
